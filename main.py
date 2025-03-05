1b6e8f36744ca88b73c15f131a9b6cbc0745e6b1
Diego <arcoespinozadiego@gmail.com>
2025-03-04 21:20:47 -0500

    Second commit, make design of app.

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 from tkinter import *
 
-print("Hello world!")
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+windows = Tk()
+windows.title("Pomodoro")
+windows.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+image = PhotoImage(file="./tomato.png")
+canvas.create_image(100, 112, image=image)
+canvas.create_text(100, 130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
+canvas.pack()
+
+windows.mainloop()
